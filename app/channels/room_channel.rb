b4a4aa548779bcbd8cf531a6440fdaefe0cda098
Hijiri K <hijiri.k.emk.p@gmail.com>
2018-01-02 08:04:22 +0900

    data create order change user -> post

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -9,9 +9,10 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
+      user = User.find_by(id: current_user.id)
+      user.group = data['group']
+      user.save
      Post.create!(content: data['message'], user_id: current_user.id, group: data['group'])
-     user = User.find_by(id: current_user.id)
-     user.group = data['group']
-     user.save
+
   end
 end
